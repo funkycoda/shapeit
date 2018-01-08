595f5ef9810aa2ebebd84fde18d4ff0a962a2d17
Ajay Ranipeta <ajay.ranipeta@gmail.com>
2018-01-08 00:43:47 -0500

    Do a convex hull of points and a simpler input file

--- shape_it.py
+++ shape_it.py
@@ -6,7 +6,7 @@ import logging
 from functools import partial
 import json
 
-from shapely.geometry import mapping, shape, Point, Polygon
+from shapely.geometry import mapping, shape, Point, Polygon, MultiPoint
 from shapely.wkt import dumps, loads
 from shapely.ops import transform as stran
 
@@ -34,6 +34,15 @@ p_out = Proj(proj_3857)
 project = partial(transform, p_out, p_in)
 output = "output"
 
+IDX_PROGRESS = 0
+
+
+class Within(object):
+    def __init__(self, o):
+        self.o = o
+    def __lt__(self, other):
+        return self.o['polygon'].within(other.o['polygon'])
+
 
 def print_exit_message(msg):
     print("\nUnable to process the file. \n Messages:\n")
@@ -60,7 +69,7 @@ def check_output_directory():
     return False
 
 
-def process_file(csvfile, colno):
+def process_file_with_species_column(csvfile, colno):
     logging.info("Processing file: %s" % csvfile)
     print("Processing file: %s" % csvfile)
 
@@ -185,6 +194,147 @@ def generate_shapefile_point(sciname, lat, lng):
     logging.info("Generated point shapefile")
 
 
+def process_file_with_latlon(csvfile, lngcol, latcol):
+    global IDX_PROGRESS
+
+    logging.info("Processing file: %s", csvfile)
+    print("Processing file: %s" % csvfile)
+
+    lyrname = os.path.basename(csvfile)
+    outfilename = os.path.splitext(lyrname)[0]
+    lyrname = outfilename
+
+    with open(csvfile) as f:
+        reader = csv.DictReader(f, skipinitialspace=True)
+        rows = list(reader)
+
+        coords = []
+        points = []
+        for i, row in enumerate(rows):
+            IDX_PROGRESS += 1
+            update_progress(IDX_PROGRESS)
+            coords.append([float(row[lngcol]), float(row[latcol])])
+            points.append(Point([float(row[lngcol]), float(row[latcol])]))
+
+        # Sort the coordinates so it's easier to match them
+        scoords = sorted(coords, key=lambda crd: crd[1])
+        scoords = sorted(scoords, key=lambda crd: crd[0])
+
+        output_geoms = get_multi_polygons(scoords)
+
+        write_shapefile(lyrname, output_geoms)
+
+    # clean up the progress bar
+    sys.stdout.write("\n")
+    print("Completed.\nCheck shape_it.log for additional details")
+
+
+def get_multi_polygons(coords):
+    global IDX_PROGRESS
+
+    logging.info("Processing %d points", len(coords))
+
+    tcoords = coords
+
+    polygons = [{
+        'polygon': None,
+        'coords': []
+    }]
+    poly_coords = []
+    pts_coords = []
+
+    # 1. Take the first 2 set of coords and
+    # create a polygon if close <80km
+    # or create 2 polygons
+    c1 = tcoords[0]
+    c2 = tcoords[1]
+
+    if not is_feature_far(Point(c1), Point(c2)):  # 0.720
+        polygons[0]['polygon'] = MultiPoint([c1, c2]).convex_hull
+        polygons[0]['coords'].append(c1)
+        polygons[0]['coords'].append(c2)
+    else:
+        polygons[0]['polygon'] = MultiPoint([c1]).convex_hull
+        polygons[0]['coords'].append([c1])
+        polygons.append({'polygon': MultiPoint([c2]).convex_hull, 'coords': [c2]})
+
+    # 2. Starting with the 3rd coord, check which polygon it falls in and add
+    # that point
+    for crd in tcoords[2:]:
+        IDX_PROGRESS += 1
+        update_progress(IDX_PROGRESS)
+        coord_added = False
+        for i, poly in enumerate(polygons):
+            pp = Point(crd[0], crd[1])
+            d = poly['polygon'].distance(pp) * 111.111
+            is_far = is_feature_far(poly['polygon'], pp)
+            if not is_far:
+                ecoords = poly['coords']
+                ecoords.append(crd)
+                poly['polygon'] = MultiPoint(ecoords).convex_hull
+                coord_added = True
+                break
+
+        if not coord_added:
+            polygons.append({'polygon': MultiPoint([crd]).convex_hull, 'coords': [crd]})
+
+    # 3. Check and merge any polygons that are <80km
+    polygons = check_and_merge_polygons(polygons)
+
+    logging.info("Generated buffered point")
+    return polygons
+
+def check_and_merge_polygons(polygons):
+    global IDX_PROGRESS
+
+    logging.info('Check and sort...')
+    # if it's a single polygon, we are good
+    if len(polygons) == 1:
+        return polygons
+
+    spolys = sorted(polygons, key=Within)
+    out_polygons = [spolys[0]]
+    for poly in spolys[1:]:
+        IDX_PROGRESS += 1
+        update_progress(IDX_PROGRESS)
+        poly_merged = False
+        for idx, opoly in enumerate(out_polygons):
+            if not is_feature_far(opoly['polygon'], poly['polygon']):
+                opoly['coords'] = opoly['coords'] + poly['coords']
+                opoly['polygon'] = MultiPoint(opoly['coords'] + poly['coords']).convex_hull
+                poly_merged = True
+
+        if not poly_merged:
+            out_polygons.append(poly)
+
+    return out_polygons
+
+
+def write_shapefile(lyrname, features):
+    logging.info("Processing layer: %s ", lyrname)
+
+    output_path = "%s/%s/" % (output, lyrname)
+    output_file = "%s/%s.shp" % (output_path, lyrname)
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+
+    schema = {'geometry': 'Polygon', 'properties': {}}
+    with fiona.open(output_file, 'w', crs=proj_4326, driver='ESRI Shapefile', schema=schema) as out:
+
+        for feature in features:
+            feature['polygon'] = feature['polygon'].buffer(0.20)
+            out.write({
+                'properties': {},
+                'geometry': mapping(feature['polygon'])
+            })
+
+    logging.info("Generated polygon shapefile")
+
+
+def is_feature_far(f1, f2, dist=80):
+    return (f1.distance(f2) * 111.111) > dist
+
+
 def update_progress(iprg):
     sys.stdout.write('\r[{0}]'.format('#'*(iprg/2)))
     sys.stdout.flush()
@@ -197,11 +347,17 @@ if __name__ == '__main__':
     parser.add_argument("input", metavar="input", help="Input CSV file")
 
     # optional arguments
-    parser.add_argument("-g", "--group", metavar="group", dest="group", type=int, default=2, help="Group column, e.g. scientific name. Default: 3.")
-    parser.add_argument("-o", "--output", metavar="output", dest="output", default=output, help="Output directory. (default: ./output/)")
+    parser.add_argument("-o", "--output", dest="output", default=output,
+                        help="Output directory. (default: ./output/)")
+    parser.add_argument("-g", "--group", dest="group", type=int, default=2,
+                        help="Group column, e.g. scientific name. Default: 3.")
+    parser.add_argument("-lon", "--longitude", dest="lon", default='longitude',
+                        help="Longitude column name, e.g. longitude. Default: longitude.")
+    parser.add_argument("-lat", "--latitude", dest="lat", default='latitude',
+                        help="Latitude column name, e.g. latitude. Default: latitude.")
 
     args = parser.parse_args()
     output = args.output
     folder_checks_out = check_output_directory()
     if folder_checks_out:
-        process_file(args.input, args.group)
+        process_file_with_latlon(args.input, args.lon, args.lat)
